d23cb0519e33f5bbda6a3d7f57ca32879e1b36d2
Nicky Marino <nicky@nickymarino.com>
2022-11-20 23:42:40 -0500

    Display a dynamo column

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,33 +1,64 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useState,
+  Component,
+  ReactNode,
+  FunctionComponent,
+  PropsWithChildren,
+} from "react";
 import useSWR, { SWRResponse, Fetcher } from "swr";
+import { Board } from "./api/board";
 import { Document } from "./api/scratch";
 
 export default function App() {
-  return <Content />;
+  return (
+    <div>
+      <Content />
+    </div>
+  );
 }
 
 function Content() {
-  const fetcher = async (slug: string): Promise<Document> =>
+  const fetcher = async (slug: string): Promise<Board> =>
     await fetch(slug).then((res) => res.json());
 
-  const { data, error } = useSWR("/api/scratch", fetcher);
+  const { data, error } = useSWR("/api/board", fetcher);
 
   if (error) return <p>Loading failed...</p>;
   if (!data) return <h1>Loading...</h1>;
 
+  console.log({ data, error });
+
+  const column = data.columns[0];
+
   return (
-    <ul>
-      {data.cards.map((card) => (
-        <Card key={card.id} title={card.title} />
-      ))}
-    </ul>
+    <div className="content">
+      <Column title={column.title} />
+      <ul>
+        {column.cards.map((card) => (
+          <Card key={card.uid} title={card.title} />
+        ))}
+      </ul>
+    </div>
   );
 }
+
+interface ColumnProps {
+  title: string;
+}
+
+const Column: React.FC<ColumnProps> = ({ title }) => {
+  return (
+    <div className="column">
+      <h1>{title}</h1>
+    </div>
+  );
+};
 
 interface CardProps {
   title: string;
 }
 
-export const Card: React.FC<CardProps> = ({ title }) => {
+const Card: React.FC<CardProps> = ({ title }) => {
   return <li>{title}</li>;
 };
