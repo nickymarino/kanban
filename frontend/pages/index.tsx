ce51de7329849d40745dcbf359a790925e454355
Nicky Marino <nicky@nickymarino.com>
2022-11-20 23:51:31 -0500

    Refactor into components

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,8 @@
-import React, {
-  useEffect,
-  useState,
-  Component,
-  ReactNode,
-  FunctionComponent,
-  PropsWithChildren,
-} from "react";
-import useSWR, { SWRResponse, Fetcher } from "swr";
+import React from "react";
+import useSWR from "swr";
+import { Column } from "../components/Card";
+import { Card } from "../components/Column";
 import { Board } from "./api/board";
-import { Document } from "./api/scratch";
 
 export default function App() {
   return (
@@ -29,36 +23,17 @@ function Content() {
 
   console.log({ data, error });
 
-  const column = data.columns[0];
-
   return (
     <div className="content">
-      <Column title={column.title} />
-      <ul>
-        {column.cards.map((card) => (
-          <Card key={card.uid} title={card.title} />
-        ))}
-      </ul>
+      {data.columns.map((column) => (
+        <Column key={column.uid} title={column.title}>
+          <ul>
+            {column.cards.map((card) => (
+              <Card key={card.uid} title={card.title} />
+            ))}
+          </ul>
+        </Column>
+      ))}
     </div>
   );
 }
-
-interface ColumnProps {
-  title: string;
-}
-
-const Column: React.FC<ColumnProps> = ({ title }) => {
-  return (
-    <div className="column">
-      <h1>{title}</h1>
-    </div>
-  );
-};
-
-interface CardProps {
-  title: string;
-}
-
-const Card: React.FC<CardProps> = ({ title }) => {
-  return <li>{title}</li>;
-};
